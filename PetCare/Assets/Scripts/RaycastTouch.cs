515aae7f0c214a3cba756f70e92ce9ae7a85dc62
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2021-12-06 13:24:08 -0300

    Adding new scripts + reverting changes + adjusting game scene

--- PetCare/Assets/Scripts/RaycastTouch.cs
+++ PetCare/Assets/Scripts/RaycastTouch.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class RaycastTouch : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] food;
-
-    private Vector3 touchPosWorld;
-
-    private TouchPhase touchPhase = TouchPhase.Ended;
-
-    // Start is called before the first frame update
     void Start()
     {
        
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -38,8 +28,4 @@ public class RaycastTouch : MonoBehaviour
         }
     }
 
-    public void InstantiateFood(Collider food)
-    {
-       
-    }
 }
